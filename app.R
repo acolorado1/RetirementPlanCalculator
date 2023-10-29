f9d02753fc427cc1378e87c71a6a55da4a9bd3a2
acolorado1 <burkhang@megamind-mac.ucdenver.pvt>
2023-10-29 17:37:28 -0600

    functioning calculator and text output

--- app.R
+++ app.R
@@ -14,19 +14,19 @@ ui <- fluidPage(theme = shinytheme("united"),
                                                    value = 50000),
                                       numericInput(inputId = "C", 
                                                    label = "Employee Bi-Weekly Contribution (Percent of Salary)", 
-                                                   value = 0.03, 
+                                                   value = 3, 
                                                    min = 0, 
-                                                   max = 1, 
-                                                   step = 0.01),
+                                                   max = 100, 
+                                                   step = 0.1),
                                       numericInput(inputId = "Y", 
                                                    label = "Years Till Retirement", 
                                                    value = 20), 
                                       numericInput(inputId = "AIR", 
-                                                   label = "Annual Interest Rate", 
-                                                   value = 0.05, 
+                                                   label = "Annual Interest Rate (Percent)", 
+                                                   value = 5, 
                                                    min = 0, 
-                                                   max = 1, 
-                                                   step = 0.001),
+                                                   max = 100, 
+                                                   step = 0.01),
                                       actionButton(
                                         "submitbutton", 
                                         "Submit", 
@@ -34,7 +34,15 @@ ui <- fluidPage(theme = shinytheme("united"),
                                     ), 
                              mainPanel(tags$label(h3('Results')), 
                                        tags$head(tags$style(HTML("pre { white-space: pre-wrap; word-break: keep-all; }"))),
-                                       verbatimTextOutput("summary"),
+                                       htmlOutput("summary"),
+                                       # tags$img(
+                                       #   src="logo.png", 
+                                       #   width = "20%", 
+                                       #   style="vertical-align:bottom"
+                                       # )
+                                       imageOutput("logo"),
+                                       imageOutput("luci")
+
                              )
                          ), #tab panel: home 
                tabPanel("About", 
@@ -47,57 +55,47 @@ ui <- fluidPage(theme = shinytheme("united"),
 )# fluid page 
 
 server <- function(input, output, session){
-  numbersInput <- reactive({
-    # Assumption: 52 weeks in a year -> 26 contributions per year 
-    
-    # calculate bi-weekly contribution (bwc)
-    ## (Annual Income * Percent of Salary)/26  
-    bwc <- round((input$AI*input$C)/26, 2)
-    
-    # calculate contribution by employer (ec) 
-    ## default is 50% of employee contribution with a max of 5% of salary 
-    if(input$C > 0.08){ 
+  # calculate bi-weekly contribution by employee 
+  BiWeekEmployee <- reactive({
+    round((input$AI*(input$C/100))/26, 2)
+  })
+  
+  # calculate bi-weekly contribution by employer 
+  BiWeekCompany <- reactive({
+    if(input$C > 8){ 
       ec <- (input$AI*0.04)/26
     }else{
-      ec <- bwc/2
+      ec <- round((input$AI*(input$C/100))/26, 2)/2
     }
-    ec <- round(ec,2)
+      round(ec,2)
+  })
+  
+  # calculate total contributed by employee 
+  TotalEmployee <- reactive({
+    round(input$AI*(input$C/100)*input$Y, 2)
+  })
+  
+  # calculate balance upon retirement
+  ## compound interest equation: BUR = PV(1+i)^n
+  ## PV = present value (first deposit)
+  ## i = rate of interest 
+  ## n = number of periods (number of contributions)
+  ## 26 contributions per year
+  BalanceUponRetirement <- reactive({
+    bwe <- input$AI*(input$C/100)
+    bwc <- isolate(BiWeekCompany())*26
+    PV <- round(bwe + bwc, 2)
+    round(PV*((1+(input$AIR/100))^(1:input$Y)),2)
     
-    # number of contributions (nc)
-    ## years till retirement * 26 
-    nc <- input$Y*26
-    
-    # calculate balance upon retirement (BUR)
-    ## compound interest equation: BUR = PV(1+i)^n
-    ## PV = present value (first deposit)
-    ## i = rate of interest 
-    ## n = number of periods (number of contributions - 1)
-    PV <- round(bwc + ec, 2)
-    principal <- round(PV*((1+input$AIR)^(0:nc-1)),2)
-    interest <- principal*input$AIR
-    BUR <- sum(interest)
-    
-    # calculate total contribution by employee (tce)
-    tce <- (input$AI*input$C)*input$Y
-    
-    print(paste0("bi-weekly contributions: ", bwc))
-    print(paste0("bi-weekly employer contribution: ", ec))
-    print(paste0("number of contributions: ", nc))
-    print(paste0("installment value: ", PV))
-    
-    
-    #output text summary: balance upon retirement, total contribution by employee
-    summary_statement <- paste0("Over the course of ",
-                                input$Y,
-                                " years, the employee will have contributed $",
-                                tce, " and their balance upon retirement will be $", BUR)
-    print(summary_statement)
   })
   
   output$summary <- renderText({
     if(input$submitbutton > 0){
-      isolate(numbersInput())
-      
+      str1 <- paste0("Bi-weekly contribution by employee: $", isolate(BiWeekEmployee()))
+      str2 <- paste0("Bi-weekly contribution by company: $", isolate(BiWeekCompany()))
+      str3 <- paste0("Total Contribution by employee after ", input$Y, " years: $", isolate(TotalEmployee()))
+      str4 <- paste0("Balance Upon Retirement: $", sum(isolate(BalanceUponRetirement())))
+      HTML(paste(str1, str2, str3, str4, sep = "<br/>"))
     }
   })
 }
